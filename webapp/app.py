df54c3a6a99a0c94adf75d5202c807e49221d320
Julie Muzina <julie.muzina@canonical.com>
2024-05-16 16:56:44 -0400

    GHA migration proof of concept

---
+++ webapp/app.py
@@ -0,0 +1,23 @@
+# Packages
+import talisker.requests
+import requests
+import flask
+import jinja2
+from canonicalwebteam.flask_base.app import FlaskBase
+
+
+app = FlaskBase(
+    __name__,
+    "0.0.0.0",
+    template_folder="../templates",
+    static_folder="../templates/static",
+)
+session = talisker.requests.get_session()
+
+@app.route("/")
+def index():
+    return flask.make_response(
+        flask.render_template(
+            "index.html"
+        )
+    )
