4c92017a99089b8c21fd1ab14747bdf8d0fdd32e
Julie Muzina <julie.muzina@canonical.com>
2024-05-16 14:36:50 -0400

    fix percy localhost maybe

--- snapshots.js
+++ snapshots.js
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT || 8101;
 
 module.exports = () => [{
-    url: `http://localhost:${PORT}/`,
+    url: `http://0.0.0.0:8101/`,
     name: 'Index',
     widths: [375]
 }]
